63b9c4743ad3c9d70b749ea6ddcd67ff27f0b031
OTT55 <ottjesse55@gmail.com>
2026-03-17 09:28:03 +0100

    nk

---
+++ learning/Ccourse/test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int x, y;
+    int z;
+    float r = 34.5;
+    float set;
+    set = r*x;
+
+    scanf("%d", &z);
+    scanf("%d", &y);
+    scanf("%d", &x);
+    printf("The answer is: ", set);
+    return 0;
+
+
+}
--- learning/pcc/aicourse.py
+++ learning/pcc/aicourse.py
@@ -1,3 +1,4 @@
 Participants = ['John', 'Seraphina', 'Blyke', 'Arlo', 'Rei', 'Isen', 'Kuyo']
 Participants 
 print(Participants[1:3])
+Participants[:-1]
