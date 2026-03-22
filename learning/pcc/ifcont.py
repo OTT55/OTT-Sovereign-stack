0eaf336a06b059b33765bdafddac69ee5a80e6f8
OTT55 <ottjesse55@gmail.com>
2026-03-22 19:06:57 +0100

    cg

---
+++ learning/pcc/ifcont.py
@@ -0,0 +1,8 @@
+ages = [12, 18, 25, 30, 40, 33, 67]
+weights = [50, 60, 70, 80, 90, 100, 110]
+heights = [150, 160, 170, 180, 190, 200, 210]
+
+if 12 in ages:
+    print("I am OTT")
+if 160 not in ages:
+    print("You can live")
