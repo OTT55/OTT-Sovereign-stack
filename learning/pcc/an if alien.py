a3d2a27983bca07e7aa3406c25f0e3ed692e9c6b
OTT55 <ottjesse55@gmail.com>
2026-03-29 20:30:37 +0100

    up on date

---
+++ learning/pcc/an if alien.py
@@ -0,0 +1,15 @@
+alien_color = input("Enter the colour of the alien:\n")
+if alien_color == 'green':
+    print("Player just earned five points.")
+elif alien_color == 'yellow':
+    print("Player just earned ten points.")
+elif alien_color == 'red':
+    print("Player just earned fifteen points.")
+
+fav_fruits= ['apples', 'mangoes', 'oranges']
+if 'bananas' in fav_fruits:
+    print("You really like Bananas.")
+if 'oranges' in fav_fruits:
+    print("You really like oranges.")
+if 'grapes' in fav_fruits:
+    print("You reallyt like grapes.")
