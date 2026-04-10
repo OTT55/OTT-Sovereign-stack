646ff94cf6696e81ef1bb7c3e7bec12f6398de37
OTT55 <ottjesse55@gmail.com>
2026-04-10 04:25:33 +0100

    I'm back udemy

--- learning/pcc/ai.py
+++ learning/pcc/ai.py
@@ -1,19 +1,32 @@
-prices = {
-    "box_of_spaghetti" : 4,
-    "lasagna" : 5,
-    "hamburgers" : 2
-}
-quantity = {
-    "box_of_spaghetti" : 6, 
-    "lasagna" : 10,
-    "hamburgers" :0
-}
-money_spent = 0
-
-
-for ott in prices:
-    money_spent = money_spent + (prices[ott] * quantity[ott])
-print(money_spent)
-
-new_list_comprehension = [i+j for i in range(200) for j in range(50)]
-print(new_list_comprehension, end = " ")
+alien_0 = {'color': "green", 'points': "5"}
+print(alien_0['color'])
+new_points = alien_0['points']
+print(f"You just earned {new_points} points!")
+alien_0['x-position']= "56"
+alien_0['y-position']= "90"
+print(alien_0)
+print(alien_0['x-position'])
+del alien_0['points']
+print(alien_0)
+alien_0 = {"x_position": 0, "y_position": 25, "speed": 'fast' }
+print(f"Original position: {alien_0['x_position']}")
+#move the alien to the right.
+#Determine how far to move the alien based on iys current speed."
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    #This must be a fast alien. 
+    x_increment = 3
+#The new position is the old position plus the incremengt.
+alien_0['x_position']= alien_0['x_position'] + x_increment
+print(f"New position: {alien_0['x_position']}")
+fav_languages = {
+    "pascal": 'python',
+    "rain": 'c++',
+    "jules": 'rust',
+    "taylor": 'php',
+    }
+language = fav_languages['taylor'].title()
+print(f"Sarah's favorite language is {language}")
--- learning/pcc/an if alien.py
+++ learning/pcc/an if alien.py
@@ -13,3 +13,44 @@ if 'oranges' in fav_fruits:
     print("You really like oranges.")
 if 'grapes' in fav_fruits:
     print("You reallyt like grapes.")
+
+    prices = {
+    "box_of_spaghetti" : 4,
+    "lasagna" : 5,
+    "hamburgers" : 2
+}
+quantity = {
+    "box_of_spaghetti" : 6, 
+    "lasagna" : 10,
+    "hamburgers" :0
+}
+money_spent = 0
+
+
+#for ott in prices:
+ #   money_spent = money_spent + (prices[ott] * quantity[ott])
+#print(money_spent)
+
+#new_list_comprehension = [i+j for i in range(200) for j in range(50)]
+#print(new_list_comprehension, end = " ")
+x = "JOHN"
+print(x.capitalize())
+current_users = ['Admin', 'Njinwi', 'Jesse', 'Ngu-Ngwa', 'OTT', 'Prime']
+new_users = ['Gregory', 'Bruno', 'Jesse', 'Christiano', 'Messi', 'Njinwi']
+#user = input("Enter your username:\n")
+for new in new_users:
+    if new.capitalize() in current_users:
+        print("That username is not available. Please enter another username.")
+    else:
+        print("That username is available.")
+    
+ordinal_nummbers= [1,2,3,4,5,6,7,8,9]
+for number in ordinal_nummbers:
+    if number == 1:
+        print(f"{number}st")
+    elif number == 2:
+        print(f"{number}nd")
+    elif number == 3:
+        print(f"{number}rd")
+    else:
+        print(f"{number}th")
