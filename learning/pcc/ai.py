04f3f1466b8c01ae2f30d3eea74287b7bdd7b100
OTT55 <ottjesse55@gmail.com>
2026-05-07 18:01:45 +0100

    tyyy

--- learning/pcc/ai.py
+++ learning/pcc/ai.py
@@ -22,3 +22,17 @@ for username, user in users.items():
         location = user['location']
         print(f"\tFull name: {full_name.title()}")
         print(f"\tLocation : {location.title()}")
+
+num = 1
+while(num<100000):
+        print(num, end=" ")
+
+        num += 1
+
+sum = 0 
+num = 1
+while(num<10):
+    sum += num
+    num += 1
+print(f"\nThe sum of 1-10 numbers is {sum}")
+
